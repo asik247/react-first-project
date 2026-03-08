8b4d735f01dd8569334f63879edbdacdd508e6b8
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-08 12:18:07 +0600

    nested router implement

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <h1>Banner this is index true</h1>
+        </div>
+    );
+};
+
+export default Hero;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from "react-router";
+import Root from "../Root/Root";
+import Hero from "../Components/Hero/Hero";
+import Profile from "../Components/Profile/Profile";
 
 export const router = createBrowserRouter([
     {
         path:"/",
-        element:<div>Hello React World</div>
+        Component:Root,
+        children:[
+            {index:true,Component:Hero},
+            {path:"profile",Component:Profile}
+            
+        ]
     }
 ])
